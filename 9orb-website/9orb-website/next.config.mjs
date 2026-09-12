29188266583900badfb65ef7fca540af6e577484
Nitish Vankina <mr.freelancer9182@gmail.com>
2026-09-11 23:09:15 -0400

    delete initial

--- 9orb-website/9orb-website/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  eslint: {
-    dirs: ['app', 'components', 'lib'],
-  },
-};
-
-export default nextConfig;
